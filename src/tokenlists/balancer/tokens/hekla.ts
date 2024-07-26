11027a4affff3ee643234345ed61bd689bede528
James Russell Orola <jrmorola@gmail.com>
2024-07-26 15:43:59 +0800

    feat: add hekla WETH token

--- src/tokenlists/balancer/tokens/hekla.ts
+++ src/tokenlists/balancer/tokens/hekla.ts
@@ -1,4 +1,7 @@
 export default [
+  // official
+  '0x0be4cb1416e178177e7ef19dd199860e755213e7', // WETH
+
   // mock tokens
   '0x961A0ae05CC348bfd24eE49318F40D6701245f2d', // USDC
   '0x2c843AbDE5e92253D7f20F14B70E3CBE6672dadd', // XSGD
